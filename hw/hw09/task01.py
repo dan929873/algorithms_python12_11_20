e859af58b2df0755b761dc522bae417d58f9d959
dan929873 <metallica04@yandex.ru>
2020-12-29 17:48:17 +0300

    hw09.2

--- hw/hw09/task01.py
+++ hw/hw09/task01.py
@@ -3,3 +3,16 @@
 # Примечания:
 # * в сумму не включаем пустую строку и строку целиком;
 # * без использования функций для вычисления хэша (hash(), sha1() или любой другой из модуля hashlib задача считается не решённой.
+
+def count_sub(s:str):
+    set_hash = set()
+
+    for i in range(len(s) - 1):
+        for j in range(i + 1, len(s) + 1):
+            set_hash.add(hash(s[i:j]))
+
+    count = len(set_hash) - 1
+    return count
+
+my_str = input("Введите строку: ")
+print(f'В строке: "{my_str}" есть {count_sub(my_str)} различных подстрок')
